1fca26524d2f9169abbfed81bdfbe6e02d3a17bc
Jesús Ruiz-Ayúcar <chusete@gmail.com>
2020-11-06 23:49:23 +0100

    Rename rest/fetch_document resources from product to article

--- rest/fetch_document/index.js
+++ rest/fetch_document/index.js
@@ -1,15 +1,26 @@
-var express = require('express');
+/**
+ * To fetch a document run any of the following:
 
-var app = express();
+curl -v http://localhost:4000/33b27e35-8f1a-4e05-8c4a-e36a90cffd9b
+
+curl -v http://localhost:4000/articles/12
+
+ * The above request will return the article whose id is `12`: `Article 12`
+ *
+ */
+
+const express = require('express');
+const app = express();
+
 app
-    // Return a product from an opaque URI.
+    // Return an article from an opaque URI.
     .get('/:id([a-z0-9\-]+)', (req, res) => res.format({
-        'text/plain': () => res.send(`Get a product with id ${req.params.id}`)
+        'text/plain': () => res.send(`Article ${req.params.id}`)
     }))
 
-    // Return a product using the most commonly used URI convention.
-    .get('/products/:id', (req, res) => res.format({
-        'text/plain': () => res.send(`Get a product with id ${req.params.id}`)
+    // Return an article using the most commonly used URI convention.
+    .get('/articles/:id', (req, res) => res.format({
+        'text/plain': () => res.send(`Article ${req.params.id}`)
     }));
 
 app.listen(4000, () => console.log('Running a REST server at http://localhost:4000/'));
